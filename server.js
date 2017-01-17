6d39c0606f5d27d04ee0f1fe54cf36225f7cd6c5
Cian Duffy <cianduffyty@gmail.com>
2017-01-17 15:40:39 +0000

    UI and key commands

--- server.js
+++ server.js
@@ -2,15 +2,36 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+// var ffmpeg = require('ffmpeg');
+// var dronestream = require("dronestream").listen(server);
 
 users = [];
 connections = [];
 
-server.listen(process.env.PORT || 3000);
+var isFlying, strafingLeft, strafingRight, strafingForward, strafingBackward, rotatingClockwise, rotatingAntiClockwise, descending, ascending = false;
+
+server.listen(3000);
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname +'/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
+
+// io.sockets.on('connection', function(socket) {
+//     // Connect
+//     connections.push(socket);
+//     console.log('Connected: %s sockets connected', connections.length);
+//
+//     // Disconnect
+//     socket.on('disconnect', function (data) {
+//         connections.splice(connections.indexOf(socket), 1);
+//         console.log('Disconnected: %s sockets connected', connections.length);
+//     });
+//
+//     socket.on('key-pressed', function (data) {
+//     	console.log('\nKey pressed: ');
+// 		console.log(data);
+//     });
+// });
 
 io.sockets.on('connection', function(socket) {
 	// Connect
@@ -25,7 +46,6 @@ io.sockets.on('connection', function(socket) {
 
 	// Connect to AR.Drone
 	socket.on('connect-to-drone', function(debugMode) {
-		
 		console.log(debugMode);
 
 		console.log('Connect to Drone');
@@ -33,24 +53,185 @@ io.sockets.on('connection', function(socket) {
 			var arDrone = require('ar-drone');
 			var client  = arDrone.createClient();
 		}
-		
+
 		// Drone Commands
 
-		// Takeoff
-		socket.on('takeoff', function() {
-			console.log('Take off');
-			if (debugMode == false) {
-				client.takeoff();
-			}
+		// Takeoff / land
+		socket.on('takeoff-or-land', function() {
+            if (!isFlying){
+                console.log('takeoff()');
+                isFlying = true;
+                if (!debugMode) {
+                    client.takeoff();
+                }
+            } else {
+                console.log('land');
+                isFlying = false;
+                if (!debugMode) {
+                    client.stop();
+                    client.land();
+                }
+            }
 		});
 
-		// Land
-		socket.on('land', function(){
-			console.log('Land');
-			if (debugMode == false) {
-				client.stop();
-				client.land();
-			}
-		});
+		socket.on('strafe-left', function (start) {
+            if (start == true) {
+                // start moving left
+                if (!strafingLeft) {
+                    console.log('start strafing left');
+                    strafingLeft = true;
+                    if (!debugMode) {
+                        // strafe left
+                    }
+                }
+            } else {
+                // stop moving left
+                console.log('stop strafing left');
+                strafingLeft = false;
+                if (!debugMode) {
+                    // stop strafing left
+                }
+            }
+        });
+
+        socket.on('strafe-right', function (start) {
+            if (start == true) {
+                // start moving right
+                if (!strafingRight) {
+                    console.log('start strafing right');
+                    strafingRight = true;
+                    if (!debugMode) {
+                        // strafe right
+                    }
+                }
+            } else {
+                // stop moving right
+                console.log('stop strafing right');
+                strafingRight = false;
+                if (!debugMode) {
+                    // stop strafing right
+                }
+            }
+        });
+
+        socket.on('strafe-forward', function (start) {
+            if (start == true) {
+                // start moving forward
+                if (!strafingForward) {
+                    console.log('start strafing forward');
+                    strafingForward = true;
+                    if (!debugMode) {
+                        // strafe forward
+                    }
+                }
+            } else {
+                // stop moving forward
+                console.log('stop strafing forward');
+                strafingForward = false;
+                if (!debugMode) {
+                    // stop strafing forward
+                }
+            }
+        });
+
+        socket.on('strafe-back', function (start) {
+            if (start == true) {
+                // start moving backward
+                if (!strafingBackward) {
+                    console.log('start strafing backward');
+                    strafingBackward = true;
+                    if (!debugMode) {
+                        // strafe forward
+                    }
+                }
+            } else {
+                // stop moving backward
+                console.log('stop strafing backward');
+                strafingBackward = false;
+                if (!debugMode) {
+                    // stop strafing backward
+                }
+            }
+        });
+
+        socket.on('ascend', function (start) {
+            if (start == true) {
+                // start rotating anti-clockwise
+                if (!ascending) {
+                    console.log('start ascending');
+                    ascending = true;
+                    if (!debugMode) {
+                        // strafe forward
+                    }
+                }
+            } else {
+                // stop rotating anti-clockwise
+                console.log('stop ascending');
+                ascending = false;
+                if (!debugMode) {
+                    // stop rotating clockwise
+                }
+            }
+        });
+
+        socket.on('descend', function (start) {
+            if (start == true) {
+                // start rotating anti-clockwise
+                if (!rotatingAntiClockwise) {
+                    console.log('start descending');
+                    descending = true;
+                    if (!debugMode) {
+                        // strafe forward
+                    }
+                }
+            } else {
+                // stop rotating anti-clockwise
+                console.log('stop descending');
+                descending = false;
+                if (!debugMode) {
+                    // stop rotating clockwise
+                }
+            }
+        });
+
+        socket.on('rotate-clockwise', function (start) {
+            if (start == true) {
+                // start rotating clockwise
+                if (!rotatingClockwise) {
+                    console.log('start rotating clockwise');
+                    rotatingClockwise = true;
+                    if (!debugMode) {
+                        // strafe forward
+                    }
+                }
+            } else {
+                // stop rotating clockwise
+                console.log('stop rotating clockwise');
+                rotatingClockwise = false;
+                if (!debugMode) {
+                    // stop rotating clockwise
+                }
+            }
+        });
+
+        socket.on('rotate-anti-clockwise', function (start) {
+            if (start == true) {
+                // start rotating anti-clockwise
+                if (!rotatingAntiClockwise) {
+                    console.log('start rotating anti-clockwise');
+                    rotatingAntiClockwise = true;
+                    if (!debugMode) {
+                        // strafe forward
+                    }
+                }
+            } else {
+                // stop rotating anti-clockwise
+                console.log('stop rotating anti-clockwise');
+                rotatingAntiClockwise = false;
+                if (!debugMode) {
+                    // stop rotating clockwise
+                }
+            }
+        });
 	});
 });
