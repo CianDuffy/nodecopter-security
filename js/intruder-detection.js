9ccc8d3d9b7f28fcb51becb97b6f5888d8323e4e
Cian Duffy <cianduffyty@gmail.com>
2017-03-13 10:57:14 +0000

    Added motion_detect.py

--- js/intruder-detection.js
+++ js/intruder-detection.js
@@ -5,16 +5,25 @@ server.listen(3001);
 var io = require('socket.io').listen(server);
 
 var opn = require('opn');
+
 var arDrone = require('ar-drone');
 var client  = arDrone.createClient();
 var pngStream = client.getPngStream();
 
 var cv = require('opencv');
 
+var chokidar = require('chokidar');
+var detectedWatcher = chokidar.watch('./images/intruder-detection/detected/', {
+    ignored: /[\/\\]\./, persistent: true
+});
+var droneOutputWatcher = chokidar.watch('./images/intruder-detection/drone-output/', {
+    ignored: /[\/\\]\./, persistent: true
+});
+
 var lastPNG;
 var browserOpened = false;
-var thickness = 2;
-var COLOUR_GREEN = [0, 255, 0];
+var firstLoop = true;
+var detecting = false;
 
 pngStream.on('error', console.log);
 pngStream.on('data', function (pngBuffer) {
@@ -22,27 +31,37 @@ pngStream.on('data', function (pngBuffer) {
     cv.readImage(lastPNG, function (error, image) {
         if (error) throw error;
         if (image.width() < 1 || image.height() < 1) throw new Error('Image has no size');
-        image.detectObject('./node_modules/opencv/data/haarcascade_fullbody.xml', {}, function(error, people){
-            if (error) throw error;
-
-            for (var i = 0; i < people.length; i++){
-                var person = people[i];
-                image.rectangle([person.x, person.y], [person.width, person.height], COLOUR_GREEN, thickness);
-            }
-
-            if (people.length > 0 && !browserOpened) {
-                image.save('./images/png/intruder-detected.png');
-                console.log('Image saved to ./images/png/intruder-detected.png');
-                opn('http://localhost:3000/intruder_detected');
-                browserOpened = true;
-            }
-        });
+        if (firstLoop) {
+            firstLoop = false;
+            console.log('background');
+            image.save('./images/intruder-detection/background.png');
+        } else if (!browserOpened && !detecting) {
+            detecting = true;
+            console.log('drone-output');
+            image.save('./images/intruder-detection/drone-output/intruder-detected.png');
+        }
     });
+});
+
+detectedWatcher.on('add', function(path) {
+    browserOpened = true;
+    console.log('File ', path, ' has been added');
+    setTimeout(openBrowser, 500);
+});
+
+droneOutputWatcher.on('unlink', function(path) {
+    console.log('File ', path, ' has been deleted');
+    console.log('YURT');
+    detecting = false;
 });
 
 io.sockets.on('connection', function (socket) {
     socket.on('false-alarm', function () {
-        console.log('intruder-detection.js: false alarm');
         browserOpened = false;
+        console.log('intruder-detection.js: false alarm');
     });
 });
+
+var openBrowser = function () {
+    opn('http://localhost:3000/intruder_detected');
+};
---
+++ python/motion_detect.py
@@ -0,0 +1,60 @@
+import os, cv2
+
+droneOutputString = "../images/intruder-detection/drone-output/intruder-detected.png"
+backgroundImageString = "../images/intruder-detection/background.png"
+detectedImageString = "../images/intruder-detection/detected/intruder-detected.png"
+
+minArea = 500
+backgroundSubtractor = cv2.BackgroundSubtractorMOG()
+
+
+def clear_directories():
+    if os.path.exists(droneOutputString):
+        os.remove(droneOutputString)
+    if os.path.exists(backgroundImageString):
+        os.remove(backgroundImageString)
+    if os.path.exists(detectedImageString):
+        os.remove(detectedImageString)
+
+
+def train_background_subtractor():
+    print 'training subtractor'
+    for i in range(1, 16):
+        bgImageFile = backgroundImageString.format(i)
+        bg = cv2.imread(bgImageFile)
+        backgroundSubtractor.apply(bg, learningRate=0.5)
+
+
+def detect_intruders():
+    print 'detecting intruders'
+    stillFrame = cv2.imread(droneOutputString)
+    fgmask = backgroundSubtractor.apply(stillFrame, learningRate=0)
+    
+    (cnts, _) = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    largeContours = []
+    
+    for c in cnts:
+        if cv2.contourArea(c) > minArea:
+            largeContours.append(c)
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(stillFrame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    if len(largeContours) > 0:
+        cv2.imwrite('../images/intruder-detection/detected/intruder-detected.png', stillFrame)
+    
+    print 'deleting file'
+    os.remove(droneOutputString)
+
+
+def main():
+    clear_directories()
+    while True:
+        if os.path.exists(droneOutputString):
+            print 'file exists'
+            train_background_subtractor()
+            detect_intruders()
+
+
+if __name__ == '__main__':
+    main()
